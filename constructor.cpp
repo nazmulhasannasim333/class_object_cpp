2964dec012c7d24bfc5ae1ac9ba09575ecdc33e1
Nasim <mdnasimhosen333@gmail.com>
2025-05-28 23:56:24 +0600

    Refactor formatting and style in constructor.cpp for improved readability

--- constructor.cpp
+++ constructor.cpp
@@ -1,21 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Student {
-    public:
+class Student
+{
+public:
     int roll;
     int cls;
     double gpa;
 
-    Student(int r, int c, double g) {
+    Student(int r, int c, double g)
+    {
         roll = r;
         cls = c;
         gpa = g;
     }
 };
 
-int main(){
+int main()
+{
     Student s1(101, 10, 3.5);
-    
+    cout << s1.roll << " " << s1.cls << " " << s1.gpa << endl;
     return 0;
 }

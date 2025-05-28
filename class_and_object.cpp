4390ad696d5103a11c9182824eda6169662c8302
Nasim <mdnasimhosen333@gmail.com>
2025-05-28 22:14:58 +0600

    Refactor input handling in class_and_object.cpp for improved data capture; update input.txt with full names and correct formatting; modify output.txt to reflect changes in input data.

--- class_and_object.cpp
+++ class_and_object.cpp
@@ -12,8 +12,11 @@ public:
 int main()
 {
     Student a, b;
-    cin >> a.name >> a.roll >> a.gpa;
-    cin >> b.name >> b.roll >> b.gpa;
+    cin.getline(a.name, 100);
+    cin >> a.roll >> a.gpa;
+    cin.ignore();
+    cin.getline(b.name, 100);
+    cin >> b.roll >> b.gpa;
     cout << a.name << " " << a.roll << " " << a.gpa << endl;
     cout << b.name << " " << b.roll << " " << b.gpa << endl;
     return 0;

0659e68ad08954afb632c339cabc8d3ac7943f31
Nasim <mdnasimhosen333@gmail.com>
2025-05-28 23:52:02 +0600

    Refactor Student class constructor for improved clarity and organization

--- constructor.cpp
+++ constructor.cpp
@@ -6,9 +6,16 @@ class Student {
     int roll;
     int cls;
     double gpa;
+
+    Student(int r, int c, double g) {
+        roll = r;
+        cls = c;
+        gpa = g;
+    }
 };
 
 int main(){
+    Student s1(101, 10, 3.5);
     
     return 0;
 }
